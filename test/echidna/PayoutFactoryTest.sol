9d2837c5063f57918720270f7ad9599a903b9785
ameanasad <amean.asad1999@gmail.com>
2023-02-03 16:03:39 -0500

    fix: PayoutFactory Echidna tests

--- test/echidna/PayoutFactoryTest.sol
+++ test/echidna/PayoutFactoryTest.sol
@@ -6,16 +6,14 @@ import "../../src/PaymentSplitter.sol";
 
 contract TestPayoutFactory is PayoutFactory {
 
-   address private deployerAddr;
-   constructor() PayoutFactory(deployerAddr) {
-        _grantRole(DEFAULT_ADMIN_ROLE, address(0x2000));
-        _grantRole(DEFAULT_ADMIN_ROLE, address(0x3000));
-        _grantRole(DEFAULT_ADMIN_ROLE, address(0x1000));
-        _grantRole(DEFAULT_ADMIN_ROLE, deployerAddr);
+   constructor()  PayoutFactory(address(0x00a329c0648769A73afAc7F9381E08FB43dBEA72)) payable {
+        _grantRole(DEFAULT_ADMIN_ROLE, address(0x0000000000000000000000000000000000010000));
+        _grantRole(DEFAULT_ADMIN_ROLE, address(0x0000000000000000000000000000000000020000));
+        _grantRole(DEFAULT_ADMIN_ROLE, address(0x0000000000000000000000000000000000030000));
+        _grantRole(DEFAULT_ADMIN_ROLE, address(0x00a329c0648769A73afAc7F9381E08FB43dBEA72));
     }
 
     mapping(address => bool) Addr;
-
 
     function hasNoDuplicates(address[] memory arr) private returns (bool) {
         uint256 i;
@@ -125,9 +123,11 @@ contract TestPayoutFactory is PayoutFactory {
         return true;
     }
 
-    function echidna_payout_length() public view returns(bool) {
-        return _payouts.length == 0;
-    }
+    // This function acts as a sanity check that the PayoutFactory
+    // is generating new PaymentSplitter contracts. It should always fail
+    // function echidna_payout_length() public view returns(bool) {
+    //     return _payouts.length == 0;
+    // }
 
     function echidna_released_less_total() public view returns (bool) {
         return contractReleaseLessThanTotal(_payouts);
