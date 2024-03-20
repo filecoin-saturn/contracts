32fa7d2678be68bf4bc1c4d9f7f75613779a76b5
Joao Andrade <sousa.andrade.joao@gmail.com>
2024-03-20 13:12:13 +0000

    Add revoke admin integration test

--- cli/tests/integration_tests.rs
+++ cli/tests/integration_tests.rs
@@ -142,6 +142,8 @@ fn cli_4_claim() -> Result<(), Box<dyn std::error::Error>> {
         factory_addr,
         "--addr-to-claim",
         RECIPIENT_ADDRESS,
+        "--offset",
+        "0",
     ];
     args.append(&mut new_payout_args);
 
@@ -162,8 +164,26 @@ fn cli_5_grant_admin() -> Result<(), Box<dyn std::error::Error>> {
         factory_addr,
         "--address",
         RECIPIENT_ADDRESS,
-        "--offset",
-        "0",
+    ];
+    args.append(&mut new_payout_args);
+
+    let mut cmd = Command::cargo_bin("saturn-contracts")?;
+    cmd.args(&args);
+    cmd.output().ok();
+    Ok(())
+}
+
+#[test]
+fn cli_6_revoke_admin() -> Result<(), Box<dyn std::error::Error>> {
+    let mut args = get_const_cli_args();
+
+    let factory_addr = &FACTORY_ADDRESS.lock().unwrap();
+    let mut new_payout_args = vec![
+        "revoke-admin",
+        "--factory-addr",
+        factory_addr,
+        "--address",
+        RECIPIENT_ADDRESS,
     ];
     args.append(&mut new_payout_args);
 
